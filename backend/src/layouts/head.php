46bf0802502bb8934d42364733d0388c796f2850
Serhii Bida <sergijbida15@gmail.com>
2024-10-19 21:30:26 +0300

    changed icons in menu

--- backend/src/layouts/head.php
+++ backend/src/layouts/head.php
@@ -21,10 +21,7 @@ if ($_SERVER['SCRIPT_NAME'] === '/index.php') {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="<?php echo $links['icon'] ?>">
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=login"/>
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=deployed_code"/>
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"/>
     <link rel="stylesheet" href="<?php echo $links['mainStyle'] ?>">
     <link rel="stylesheet" href="<?php echo $links['mediaStyle'] ?>">
     <title>
