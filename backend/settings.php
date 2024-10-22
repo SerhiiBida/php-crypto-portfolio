f9389d9e5ca8328f33d101db5c4393a135bb11c1
Serhii Bida <sergijbida15@gmail.com>
2024-10-22 18:03:44 +0300

    updated settings.php

--- backend/settings.php
+++ backend/settings.php
@@ -9,6 +9,15 @@ if (isset($_COOKIE['auth'])) {
     session_start();
 }
 
+if (isset($_COOKIE['auth']) && session_status() !== PHP_SESSION_NONE) {
+    // Если в сессии нет данных из куки про пользователя
+    if (!isset($_SESSION['auth'])) {
+        $_SESSION['auth'] = true;
+        $_SESSION['userId'] = intval($_COOKIE['userId']);
+        $_SESSION['username'] = $_COOKIE['username'];
+    }
+}
+
 // Подключаем библиотеки и namespace
 require __DIR__ . '/vendor/autoload.php';
 
