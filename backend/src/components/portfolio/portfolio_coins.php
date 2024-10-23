ed1a3660dcc23d03681c9dad19d46a1a1e36407c
Serhii Bida <sergijbida15@gmail.com>
2024-10-23 22:04:09 +0300

    did styles for portfolio_coins.php

--- backend/src/components/portfolio/portfolio_coins.php
+++ backend/src/components/portfolio/portfolio_coins.php
@@ -1,3 +1,82 @@
+<?php
+// Обработка фильтрации, сортировки, поиска
+// Пагинация
+?>
 <section class="portfolio-coins">
-
+    <div class="portfolio-coins-header">
+        <div class="portfolio-coins-header-title">
+            <span class="portfolio-coins-header-icon material-symbols-outlined opacity-60">
+                work
+            </span>
+            <h2>
+                Assets
+            </h2>
+        </div>
+        <?php require_once __DIR__ . '/portfolio_coins_filter_form.php'; ?>
+    </div>
+    <div class="portfolio-coins-table-wrapper">
+        <table class="portfolio-coins-table">
+            <tr class="portfolio-coins-table-header">
+                <th>
+                    Name
+                </th>
+                <th>
+                    Price
+                </th>
+                <th>
+                    Holding
+                </th>
+                <th>
+                    Avg. Buy Price
+                </th>
+                <th>
+                    Profit/Loss
+                </th>
+                <th>
+                    Invested
+                </th>
+                <th>
+                    Actions
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    Bitcoin
+                </td>
+                <td>
+                    65450 $
+                </td>
+                <td>
+                    345.567 $
+                    <br>
+                    0.000006 btc
+                </td>
+                <td>
+                    25560 $
+                </td>
+                <td>
+                    -78%
+                </td>
+                <td>
+                    1200 $
+                </td>
+                <td>
+                    <button class="portfolio-coins-table-button submit-btn">
+                        DELETE
+                    </button>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="portfolio-coins-pagination">
+        <a href="#" class="submit-btn">
+            <
+        </a>
+        <a href="#" class="submit-btn">
+            >
+        </a>
+        <p>
+            1 of 2 pages
+        </p>
+    </div>
 </section>
---
+++ backend/src/components/portfolio/portfolio_coins_filter_form.php
@@ -0,0 +1,59 @@
+<?php
+// Фильтрация
+$prices = [1 => 'default', '0 - 99 $', '100 - 499 $', '500 - 999 $', '1000 - 9999 $', '10000+ $'];
+
+// Сортировка
+$sorts = [1 => 'default', 'Name', 'Price', 'Holding', 'Avg. Buy Price', 'Profit/Loss', 'Invested'];
+?>
+<form
+        action="<?php echo $_SERVER['PHP_SELF'] . '?page-id=' . $_GET['page-id']; ?>"
+        method="post"
+        class="portfolio-coins-filter-form"
+>
+    <div class="portfolio-coins-filter-form-select-price">
+        <label>
+            Prices:
+            <select id="price" name="price">
+                <?php foreach ($prices as $key => $price): ?>
+                    <option
+                            value="<?php echo $key ?>"
+                        <?php echo formSelectValue('price', $key); ?>
+                    >
+                        <?php echo $price ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        </label>
+    </div>
+    <div class="portfolio-coins-filter-form-select-sort">
+        <label>
+            Sort
+            <select id="sort" name="sort">
+                <?php foreach ($sorts as $key => $sort): ?>
+                    <option
+                            value="<?php echo $key ?>"
+                        <?php echo formSelectValue('sort', $key); ?>
+                    >
+                        <?php echo $sort ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        </label>
+    </div>
+    <div class="portfolio-coins-filter-form-input-name custom-input-wrapper">
+        <label>
+            <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value="<?php echo formTextValue('name', 'portfolio-coins-filter-form'); ?>"
+                    min="0"
+            >
+        </label>
+    </div>
+    <input type="hidden" name="portfolio-coins-filter-form" value="true">
+    <input type="hidden" name="page-id" value="<?php echo $_GET['page-id']; ?>">
+    <button type="submit" class="portfolio-coins-filter-form-submit submit-btn">
+        Search
+    </button>
+</form>
