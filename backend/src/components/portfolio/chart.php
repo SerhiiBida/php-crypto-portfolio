62cc7f04c157b2b7970f5bd2d36c11b8932094a7
Serhii Bida <sergijbida15@gmail.com>
2024-10-27 13:53:13 +0200

    did chart

--- backend/src/components/portfolio/chart.php
+++ backend/src/components/portfolio/chart.php
@@ -1,3 +1,62 @@
+<?php
+
+use App\database\tables\CoinPortfolio;
+
+
+// Данные для графика
+$labels = [];
+$realPriceInvestmentData = [];
+$investmentData = [];
+
+function getLabels(array &$coinsData): array
+{
+    return array_map(function ($coin) {
+        return strtoupper($coin['symbol']);
+    }, $coinsData);
+}
+
+function getRealPriceInvestmentData(array &$coinsData): array
+{
+    return array_map(function ($coin) {
+        return $coin['real_price_investment'];
+    }, $coinsData);
+}
+
+function getInvestmentData(array &$coinsData): array
+{
+    return array_map(function ($coin) {
+        return $coin['investment'];
+    }, $coinsData);
+}
+
+function chart(): void
+{
+    global $labels, $realPriceInvestmentData, $investmentData;
+
+    [$portfolioId, $userId, $searchName, $sort, $filterPrice] = getDataSetting();
+
+    // Получение данных
+    $coinPortfolio = new CoinPortfolio();
+
+    $coinsData = $coinPortfolio->getCoinsForPortfolio(
+        $portfolioId,
+        $userId,
+        $searchName,
+        $sort,
+        $filterPrice
+    );
+
+    if (is_null($coinsData)) {
+        return;
+    }
+
+    $labels = getLabels($coinsData);
+    $realPriceInvestmentData = getRealPriceInvestmentData($coinsData);
+    $investmentData = getInvestmentData($coinsData);
+}
+
+chart();
+?>
 <section class="chart">
     <canvas id="myChart"></canvas>
 </section>
@@ -8,12 +67,19 @@
     new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1
-            }]
+            labels: <?php echo json_encode($labels); ?>,
+            datasets: [
+                {
+                    label: "Real cost, $",
+                    backgroundColor: "#2cf83f",
+                    data: <?php echo json_encode($realPriceInvestmentData); ?>
+                },
+                {
+                    label: "Invested, $",
+                    backgroundColor: "#2979FF",
+                    data: <?php echo json_encode($investmentData); ?>
+                }
+            ]
         },
         options: {
             scales: {
