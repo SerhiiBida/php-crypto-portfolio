083586b2f68f86906e3224013a367dab53dd1cee
Serhii Bida <sergijbida15@gmail.com>
2024-10-27 13:19:06 +0200

    doing chart

--- backend/src/components/portfolio/chart.php
+++ backend/src/components/portfolio/chart.php
@@ -1,3 +1,28 @@
 <section class="chart">
-
+    <canvas id="myChart"></canvas>
 </section>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+</script>
